e93c152e4b7cc4cff507016b9cd512e9834d4739
Pendem Nickle <nicklependem@gmail.com>
2023-08-27 15:43:04 +0530

    Update

---
+++ ATMInterfaceProject/App/Entry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMInterfaceProject.App
+{
+    class Entry
+    {
+        static void Main(string[] args)
+        {
+
+            AtMApp atmApp = new AtMApp();
+            atmApp.InitializeData();
+            atmApp.Run();
+            //Utility.PressEnterToContinue();
+
+        }
+    }
+}
